420679f3dda64fde429ecd29e8dcb6fa0a24c4f0
albenisolmos <albenisolmos88@gmail.com>
2022-03-07 13:13:22 -0400

    Fix: append() rewrite the first line always

--- lua/lets-start/box.lua
+++ lua/lets-start/box.lua
@@ -3,21 +3,21 @@ local Box = {
 	margin = 1,
 	x = 1,
 	y = 1,
-	width = 1,
+	width = 0,
+	height = 0,
 	center = false,
 	longest = 0,
 	buffer = true,
 }
 
 function Box.append(self, text)
+	self.height = self.height + 1
 	assert(self.buffer, "Error: Box needs a buffer to draw")
-	vim.api.nvim_buf_set_lines(self.buffer, self.y, -1, false, {text})
+	vim.api.nvim_buf_set_lines(self.buffer, self.height, -1, false, {text})
 end
 
 function Box.append_option(self, preffix, label, suffix)
-	assert(self.buffer, "Error: Box needs a buffer to draw")
-	vim.api.nvim_buf_set_lines(self.buffer, self.y, -1, false, {
-		Option(preffix, label, suffix, self.width)})
+	self:append(Option(preffix, label, suffix, self.width))
 end
 
 function Box.new(args)
@@ -25,6 +25,9 @@ function Box.new(args)
 
 	setmetatable(o, Box)
 	Box.__index = Box
+
+	local win = vim.api.nvim_get_current_win()
+	o.width = vim.api.nvim_win_get_height(win)
 
 	for k, v in pairs(args) do
 		if o[k] then
