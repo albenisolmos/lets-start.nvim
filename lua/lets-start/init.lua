885f8ebae09c7672fa4f87c64f417388b6b65101
albenisolmos <albenisolmos88@gmail.com>
2022-03-09 10:38:05 -0400

    Update the use of option and box class

--- lua/lets-start/init.lua
+++ lua/lets-start/init.lua
@@ -1,17 +1,23 @@
 local Box = require('lets-start.box')
+local Options = require('lets-start.options')
 local M = {}
 
 function M.init()
-	local buf = vim.api.nvim_get_current_buf() -- ...and it's buffer handle.
+	local buf = vim.api.nvim_get_current_buf()
 	local box = Box.new {
 		buffer = buf,
 		y = 0,
-		x = 0
+		x = 0,
+		center = true
 	}
 
-	box:append_option("", "New file", "[n]")
-	box:append_option("", "Open file", "[f]")
-	box:append_option("", "Close", "[q]")
+	local opts = Options.new()
+	opts.expand = 20
+	box:set_options(opts)
+	opts:add("", "New file", "[n]", {buf, 'n', 'n', '<cmd>lua vim.api.nvim_command("enew")<cr>'})
+	opts:add("", "Open file", "[f]", {buf, 'n', 'f', '<cmd>lua vim.api.nvim_command("NvimTreeOpen")<cr>'})
+	opts:add("", "Close", "[q]", {buf, 'n', 'q', '<cmd>qa<cr>'})
+	opts:done()
 end
 
 return M
